4db7d22b3a330817fe3cbb9c48b3bb3f675245fe
Sergei <oreshko-sergei@mail.ru>
2019-01-31 17:36:45 +0300

    some changes

--- BelPostRestService/src/main/java/by/compit/belpost/util/impl/ParameterCreaterImpl.java
+++ BelPostRestService/src/main/java/by/compit/belpost/util/impl/ParameterCreaterImpl.java
@@ -1,15 +1,17 @@
 package by.compit.belpost.util.impl;
 
-import by.compit.belpost.util.IDProcessor;
+import by.compit.belpost.util.ParametrCreater;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class IDProcessorImpl implements IDProcessor {
+public class ParameterCreaterImpl implements ParametrCreater {
 
     @Value("${ID.parameter}")
     private String ID_PARAMETER;
+    @Value("${code.parameter}")
+    private String CODE_PARAMETER;
 
     @Override
     public Integer getId(JSONArray jsonArray) {
@@ -22,4 +24,17 @@ public class IDProcessorImpl implements IDProcessor {
         return id;
 
     }
+
+    @Override
+    public String getCode(JSONArray jsonArray) {
+        String code = null;
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            code = jsonArray.getJSONObject(i).getString(CODE_PARAMETER);
+        }
+        System.out.println("code = " + code);
+        return code;
+    }
+
+
 }

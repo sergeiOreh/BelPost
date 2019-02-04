5b4f2ed2b634e777126299fffa54e7b36f836455
Sergei <oreshko-sergei@mail.ru>
2019-02-04 12:29:45 +0300

    2

--- BelPostRestService/src/main/java/by/compit/belpost/repository/impl/RefcursorRepositoryImpl.java
+++ BelPostRestService/src/main/java/by/compit/belpost/repository/impl/RefcursorRepositoryImpl.java
@@ -1,6 +1,7 @@
-package by.compit.belpost.repository;
+package by.compit.belpost.repository.impl;
 
 import by.compit.belpost.entity.Response;
+import by.compit.belpost.repository.RefcursorRepository;
 import by.compit.belpost.srervice.ConnectionService;
 import oracle.jdbc.OracleCallableStatement;
 import oracle.jdbc.OracleTypes;
@@ -17,16 +18,17 @@ import java.util.List;
 
 @Repository
 @Transactional
-public class RefcursorRepository {
+public class RefcursorRepositoryImpl implements RefcursorRepository {
 
     private ConnectionService connection;
 
     @Autowired
-    public RefcursorRepository(ConnectionService connection) {
+    public RefcursorRepositoryImpl(ConnectionService connection) {
         this.connection = connection;
     }
 
-    public List<Response> getResponse(String code) {
+    @Override
+    public List<Response> getResponseByCode(String code) {
 
         try {
             Connection con = connection.getConnection();
@@ -60,5 +62,79 @@ public class RefcursorRepository {
         }
         return null;
 
+    }
+
+    @Override
+    public List<Response> getResponseByLot(String login, String lotNum, String startDate, String endDate) {
+        try {
+            Connection con = connection.getConnection();
+            Response response;
+            List<Response> responseList = new ArrayList<>();
+
+            String stmt = "{? = call PKG_EXP_PORTAL.GET_OPER_4_LOT(?,?,?,?)}";
+
+            OracleCallableStatement st =
+                    (OracleCallableStatement) con.prepareCall(stmt);
+            st.registerOutParameter(1, OracleTypes.CURSOR);
+            st.setString(2, login);
+            st.setString(3, lotNum);
+            st.setString(4, startDate);
+            st.setString(5, endDate);
+            System.out.println(st.execute());
+            ResultSet rs = st.getCursor(1);
+            while (rs.next()) {
+                response = new Response();
+                response.setCode(rs.getString(1));
+                response.setOtoDate(rs.getString(2));
+                response.setOperName(rs.getString(3));
+                response.setOrgCur(rs.getString(4));
+                response.setZipCodeNext(rs.getString(5));
+                responseList.add(response);
+            }
+
+            st.close();
+            con.close();
+            return responseList;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Response> getResponseByDiapason(String codeStart, String codeFinish) {
+        try {
+            Connection con = connection.getConnection();
+            Response response;
+            List<Response> responseList = new ArrayList<>();
+
+            String stmt = "{? = call PKG_EXP_PORTAL.GET_OPER_4_DIAPASON(?,?)}";
+
+            OracleCallableStatement st =
+                    (OracleCallableStatement) con.prepareCall(stmt);
+            st.registerOutParameter(1, OracleTypes.CURSOR);
+            st.setString(2, codeStart);
+            st.setString(2, codeFinish);
+            System.out.println(st.execute());
+            ResultSet rs = st.getCursor(1);
+            while (rs.next()) {
+                response = new Response();
+                response.setCode(rs.getString(1));
+                response.setOtoDate(rs.getString(2));
+                response.setOperName(rs.getString(3));
+                response.setOrgCur(rs.getString(4));
+                response.setZipCodeNext(rs.getString(5));
+                responseList.add(response);
+            }
+
+            st.close();
+            con.close();
+            return responseList;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
--- BelPostRestService/src/main/java/by/compit/belpost/util/impl/ParameterCreatorImpl.java
+++ BelPostRestService/src/main/java/by/compit/belpost/util/impl/ParameterCreatorImpl.java
@@ -1,6 +1,6 @@
 package by.compit.belpost.util.impl;
 
-import by.compit.belpost.util.ParametrCreater;
+import by.compit.belpost.util.ParametrCreator;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,18 +8,77 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 
 @Component
-public class ParameterCreaterImpl implements ParametrCreater {
+public class ParameterCreatorImpl implements ParametrCreator {
 
     @Value("${code.parameter}")
     private String CODE_PARAMETER;
+    @Value("${login.parameter}")
+    private String LOGIN_PARAMETER;
+    @Value("${lot.num.parameter}")
+    private String LOT_NUM_PARAMETER;
+    @Value("${start.date.parameter}")
+    private String START_DATE_PARAMETER;
+    @Value("${end.date.parameter}")
+    private String END_DATE_PARAMETER;
+    @Value("${code.start.parameter}")
+    private String CODE_START_PARAMETER;
+    @Value("${code.finish.parameter}")
+    private String CODE_FINISH_PARAMETER;
 
     @Override
-    public ArrayList<String> getCodes(JSONArray jsonArray) {
-        ArrayList<String> codes = new ArrayList<>();
+    public String getCode(JSONArray jsonArray) {
         for (int i = 0; i < jsonArray.length(); i++) {
-            codes.add(jsonArray.getJSONObject(i).getString(CODE_PARAMETER));
+            return jsonArray.getJSONObject(i).getString(CODE_PARAMETER);
         }
-        return codes;
+        return null;
+    }
+
+    @Override
+    public String getLogin(JSONArray jsonArray) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            return jsonArray.getJSONObject(i).getString(LOGIN_PARAMETER);
+        }
+        return null;
+    }
+
+    @Override
+    public String getLotNum(JSONArray jsonArray) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            return jsonArray.getJSONObject(i).getString(LOT_NUM_PARAMETER);
+        }
+        return null;
+    }
+
+    @Override
+    public String getStartDate(JSONArray jsonArray) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            return jsonArray.getJSONObject(i).getString(START_DATE_PARAMETER);
+        }
+        return null;
+    }
+
+    @Override
+    public String getEndDate(JSONArray jsonArray) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            return jsonArray.getJSONObject(i).getString(END_DATE_PARAMETER);
+        }
+        return null;
+    }
+
+    @Override
+    public String getCodeStart(JSONArray jsonArray) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            return jsonArray.getJSONObject(i).getString(CODE_START_PARAMETER);
+        }
+        return null;
+    }
+
+    @Override
+    public String getCodeFinish(JSONArray jsonArray) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            return jsonArray.getJSONObject(i).getString(CODE_FINISH_PARAMETER);
+        }
+        return null;
     }
 
 
